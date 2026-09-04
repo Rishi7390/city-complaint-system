b129122c2b5ae796edbd17815c2b12892edbf331
Rishi7390 <rishi951632@gmail.com>
2026-09-04 22:04:30 +0530

    Day 1: initial Spring Boot project setup

---
+++ city-complaint-system/src/main/java/com/citycomplaint/citycomplaint/CityComplaintSystemApplication.java
@@ -0,0 +1,13 @@
+package com.citycomplaint.citycomplaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityComplaintSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityComplaintSystemApplication.class, args);
+	}
+
+}
---
+++ city-complaint-system/src/test/java/com/citycomplaint/citycomplaint/CityComplaintSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citycomplaint.citycomplaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityComplaintSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
